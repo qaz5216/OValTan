d2de5b9e79f1ed779d069c73f2ad6711bf97214e
Park-Kyoung-Min <70632234+qaz5216@users.noreply.github.com>
2023-08-02 10:38:55 +0900

    FirstCommit

---
+++ Source/OValTan/OValTanGameMode.cpp
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OValTanGameMode.h"
+#include "OValTanCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AOValTanGameMode::AOValTanGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+}
