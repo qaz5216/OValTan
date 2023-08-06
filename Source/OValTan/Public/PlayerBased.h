745bb7ee2f79898b5a50708756d39b5c4776ad90
Park-Kyoung-Min <70632234+qaz5216@users.noreply.github.com>
2023-08-06 12:57:12 +0900

    developpush

---
+++ Source/OValTan/Public/PlayerBased.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OValTanCharacter.h"
+#include "PlayerBased.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OVALTAN_API APlayerBased : public AOValTanCharacter
+{
+	GENERATED_BODY()
+	
+};
