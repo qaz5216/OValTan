6bf335ef6fd71da48fe3248a117753aebfbae31b
Park-Kyoung-Min <70632234+qaz5216@users.noreply.github.com>
2023-08-21 13:09:32 +0900

    UIUI

---
+++ Source/OValTan/TabWidget.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "TabWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OVALTAN_API UTabWidget : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(VisibleAnywhere, meta=(BindWidget), Category=MySettings)
+	class UTextBlock* text_Num;
+
+	UPROPERTY(VisibleAnywhere, meta=(BindWidget), Category=MySettings)
+	class UTextBlock* text_UserName;
+
+	UPROPERTY(VisibleAnywhere, meta=(BindWidget), Category=MySettings)
+	class UTextBlock* text_KillCount;
+};
